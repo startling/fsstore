6f75a38a4816bac92c700fef7c95b803715b9a9b
tim <tdixon51793@gmail.com>
2012-04-04 20:01:10 -0500

    basic setup.py, .gitignore, and blank __init__.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+
+setup(
+    name = "fsstore",
+    version = "0.00.0dev",
+    author = "startling",
+    author_email = "tdixon51793@gmail.com",
+    packages = ["fsstore"],
+)
