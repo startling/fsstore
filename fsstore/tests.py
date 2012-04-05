0ef0f6b9298b67ca37d02b9c2745659981bb03b9
tim <tdixon51793@gmail.com>
2012-04-04 20:41:51 -0500

    a bunch of tests for said class.

---
+++ fsstore/tests.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from tempfile import mkdtemp
+from fsstore.core import Store
+
+
+class TestInterface(unittest.TestCase):
+    def setUp(self):
+        "Initialize a Store with a temporary directory."
+        self.tempdir = mkdtemp()
+        self.fs = Store(self.tempdir)
+
+    def test_save_string(self):
+        "Test that simple string-assignment works."
+        self.fs["hello"] = "world"
+        self.assertEquals(self.fs["hello"], "world")
+        self.assertEquals(self.fs.get("hello"), "world")
+
+    def test_save_dict(self):
+        "Test that we can save a dict and get it again."
+        self.fs["dir"] = {"a": "b"}
+        self.assertEquals(self.fs["dir"]["a"], "b")
+    
+    def test_nested_dicts(self):
+        "Test that we can save nested dicts and retrieve their contents."
+        self.fs["dir"] = {"x": {"y": "z"}}
+        self.assertEquals(self.fs["dir"]["x"]["y"], "z")
+
+    def test_reassignment_dict_to_string(self):
+        """Test that nothing breaks if we assign something that used to be a
+        dict to a string.
+        """
+        self.fs["dir"] = {"x": {"y": "z"}}
+        self.fs["dir"] = "a new thing."
+        self.assertEquals(self.fs["dir"], "a new thing.")
+
+    def test_reassignment_string_to_dict(self):
+        """Test that nothing breaks if we assign something that used to be a
+        string to a dict.
+        """
+        self.fs["dir"] = "an old thing."
+        self.fs["dir"] = {"x": {"y": "z"}}
+        self.assertEquals(self.fs["dir"]["x"]["y"], "z")
